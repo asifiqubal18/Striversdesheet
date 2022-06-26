6cbd4fb7c9dd19059aaa80096a1e8d64596089a2
Asif Iqubal <57530645+asifiqubal18@users.noreply.github.com>
2022-06-26 07:47:59 +0530

    Update SetMatrixZero.cpp

--- Day1/SetMatrixZero.cpp
+++ Day1/SetMatrixZero.cpp
@@ -1,3 +1,79 @@
+T.C = O(n * m) * O(n * m) + O(n*m)
+S.C = O(1)
+
+#define INF 1e9
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+
+    for (int i = 0; i < rows; i++) {
+        for(int j=0;j<cols;j++){
+            if(matrix[i][j]==0){
+                for(int k=0;k<cols;k++){
+                   if(matrix[i][k]!=0){
+                       matrix[i][k]=INF;
+                   }
+                }
+            }
+             if(matrix[i][j]==0){
+                for(int k=0;k<rows;k++){
+                   if(matrix[k][j]!=0){
+                       matrix[k][j]=INF;
+                   }
+                }
+            }
+           
+        }
+    }
+        for (int i = 0; i < rows; i++) {
+        for(int j=0;j<cols;j++){
+            
+            if(matrix[i][j]==INF){
+                matrix[i][j]=0;
+            }
+            
+        }}
+        
+
+    }
+};
+BETTER
+T.C = O(n * m) + 2 * O(n * m)
+S.C =O(n) + O(m)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> u;
+        vector<int> v;
+        int rowsize=matrix.size();
+        int colsize=matrix[0].size();
+        for(int i=0;i<rowsize;i++){
+            for(int j=0;j<colsize;j++){
+                if(matrix[i][j]==0){
+                    u.push_back(i);
+                    v.push_back(j);
+                }
+            }
+        }
+        for(int m=0;m<u.size();m++){
+            for(int p=0;p<colsize;p++){
+            matrix[u[m]][p]=0;
+        }
+        }
+        for(int n=0;n<v.size();n++){
+          for(int q=0;q<rowsize;q++){
+            matrix[q][v[n]]=0;
+        }
+        }
+        
+    }
+};
+OPTIMAL
+T.C = O(n * m) + O(n * m)
+S.C =O(1)
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -18,3 +94,5 @@ public:
     }
     }
 };
+
+Do Upvote if this is helpful
